19c794ddf08d21612c9e8f4006858d23ddaa5a6d
Nathan Smith <nsmith@lcs.on.ca>
2022-02-22 11:47:45 -0500

    added model =  structure DadJoke

---
+++ DadJokes/Model/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  DadJokes
+//
+//  Created by Nathan Smith on 2022-02-22.
+//
+
+import Foundation
+import System
+
+// The Dadjoke structure confomrs to the Decodable Protocol. This means that we want Swift to be able to take a JSON object and 'decode' into an instance of this structure.
+struct DadJoke: Decodable {
+    let id: String
+    let joke: String
+    let status: Int
+}
